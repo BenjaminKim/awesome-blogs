6bbb9133cfa4d7c5aa43ca9d4cb3e83da37c3995
Benjamin <jehokim@live.com>
2017-01-30 02:03:03 +0900

    크롤 테스트 코

--- lib/tasks/crawl.rb
+++ lib/tasks/crawl.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'pry'
+
 module Tasks
   class Crawl
     def self.do
@@ -10,9 +13,27 @@ module Tasks
         if m && m[:author_name] && m[:url]
           puts "SUCCEED, NAME: #{m[:author_name]}, URL: #{m[:url]}, tag: #{m[:tag]}"
         else
-          puts "FAILED: #{line}"
+          if line.start_with?('#') || line.blank?
+          else
+            puts "FAILED: #{line}"
+          end
         end
       end
+    end
+
+    def self.openml
+      doc = Nokogiri::XML(open('https://raw.githubusercontent.com/sarojaba/awesome-devblog/master/Korea-Dev-RSS.opml'))
+      # puts doc.inspect
+      feeds = []
+      doc.xpath('//outline/outline').each do |link|
+        h = {}
+        h[:feed_url] = link.attr('xmlUrl')
+        h[:author_name] = link.attr('title')
+        h[:feed_type] = link.attr('type')
+        feeds << h
+      end
+
+      puts feeds.to_yaml
     end
   end
 end

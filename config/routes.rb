188566de987709f1e1b49decea09fd0b4c1e2ac6
Benjamin <jehokim@live.com>
2017-01-30 18:51:43 +0900

    default format 지정.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :feeds
+  resources :feeds, defaults: { format: :xml }
   resources :categories
 end

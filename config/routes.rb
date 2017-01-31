fda726bc0afb3c00123192ac8c77c60155cb3dec
Benjamin <jehokim@live.com>
2017-01-31 16:49:41 +0900

    root 라우팅 추가.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :feeds, defaults: { format: :xml }
-  resources :categories
+  scope defaults: { format: :xml } do
+    root 'feeds#index'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :feeds
+  end
 end

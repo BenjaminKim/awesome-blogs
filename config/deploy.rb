beded46d2a2676f0795fd14d1ae054816b01c2cc
Benjamin Kim <jehokim@live.com>
2017-10-19 13:37:17 +0900

    secret 은 링크 되도록.

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,7 @@ set :deploy_to, "/home/deploy/#{fetch(:application)}"
 
 # Default value for :linked_files is []
 #append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/secrets.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'

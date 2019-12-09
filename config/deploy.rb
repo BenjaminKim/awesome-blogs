b8495c95948a59cf11bb5aa294764e171fc895d1
Benjamin <jehokim@live.com>
2019-12-09 12:30:14 +0900

    서버 새로 셋팅

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, 'git@github.com:BenjaminKim/awesome-blogs.git'
 set :branch, 'master'
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/#{fetch(:application)}"
+set :deploy_to, "/home/benjamin/#{fetch(:application)}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh

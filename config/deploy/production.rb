c4807b8ba164cf35b0c00fa4ce46b19f63708f20
Benjamin <jehokim@live.com>
2021-06-01 21:50:26 +0900

    systemd 정리

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -64,15 +64,16 @@ set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_control_app, false
 set :puma_default_control_app, "unix://#{shared_path}/tmp/sockets/pumactl.sock"
-set :puma_conf, "#{shared_path}/puma_production.rb"
-set :puma_access_log, "#{shared_path}/log/production.log"
-set :puma_error_log, "#{shared_path}/log/production.log"
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
 set :puma_role, :app
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 set :puma_threads, [3, 3]
-set :puma_workers, 4
-set :puma_worker_timeout, 30
+set :puma_workers, 2
+set :puma_worker_timeout, nil
 set :puma_init_active_record, false
 set :puma_preload_app, false
 set :puma_daemonize, false
@@ -80,4 +81,10 @@ set :puma_plugins, []  #accept array of plugins
 set :puma_tag, fetch(:application)
 set :puma_restart_command, 'bundle exec puma'
 set :puma_service_unit_name, "puma_#{fetch(:application)}_#{fetch(:stage)}"
+set :puma_systemctl_user, :system # accepts :user
+set :puma_enable_lingering, fetch(:puma_systemctl_user) != :system # https://wiki.archlinux.org/index.php/systemd/User#Automatic_start-up_of_systemd_user_instances
+set :puma_lingering_user, fetch(:user)
+set :puma_service_unit_env_file, nil
+set :puma_service_unit_env_vars, []
+
 set :nginx_use_ssl, true

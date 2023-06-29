7632a5e9f86a6edd96bbc400b8b3087256417eda
Benjamin <jehokim@live.com>
2023-06-30 03:31:21 +0900

    master 파일 링크

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, "/home/benjamin/#{fetch(:application)}"
 
 # Default value for :linked_files is []
 #append :linked_files, 'config/database.yml', 'config/secrets.yml'
-append :linked_files, 'config/secrets.yml'
+append :linked_files, 'config/secrets.yml', 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'

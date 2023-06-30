68d8e710109f48f5896f8aaeea4f4fb25fdc0c31
Benjamin <jehokim@live.com>
2023-06-30 16:17:46 +0900

    linked 파일들 정리

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,11 +22,10 @@ set :deploy_to, "/home/benjamin/#{fetch(:application)}"
 # set :pty, true
 
 # Default value for :linked_files is []
-#append :linked_files, 'config/database.yml', 'config/secrets.yml'
-append :linked_files, 'config/secrets.yml', 'config/master.key'
+append :linked_files, 'config/master.key'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/webpacker', 'public/system', 'vendor', 'storage'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }

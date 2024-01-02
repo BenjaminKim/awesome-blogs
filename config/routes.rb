3e3d41e56fbb4f9402af25ed7576a08cc5dd8e97
Benjamin <jehokim@live.com>
2024-01-02 15:35:17 +0900

    안 쓰는 코드 정리

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   scope defaults: { format: :xml } do
     root 'feeds#index'
+    resources :feeds do
+      scope defaults: { format: :json } do
+      end
+    end
   end
 end
